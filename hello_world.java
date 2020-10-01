ee16f4d9843bc59babf69ff22f1cab923fd2dcea
vishakha868 <52974462+vishakha868@users.noreply.github.com>
2020-10-01 10:48:43 +0530

    Update hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -4,6 +4,6 @@ class HelloWorld
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello, World"); 
+        System.out.print("Hello, World"); 
     } 
 } 
